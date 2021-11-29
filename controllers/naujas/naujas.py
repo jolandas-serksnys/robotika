b29775ada311ff88a49c1a179e42402fe1d0ffb7
Jolandas Šerkšnys <jolandas.serksnys@ktu.edu>
2021-11-29 21:04:37 +0200

    algoritmas left-right

--- controllers/naujas/naujas.py
+++ controllers/naujas/naujas.py
@@ -1,7 +1,7 @@
 from controller import Robot, Motor
 import struct
 
-MAX_SPEED = 6.28
+MAX_SPEED = 6.28 / 2
 
 # Get pointer to the robot.
 robot = Robot()
@@ -26,51 +26,45 @@ rightWheel.setPosition(float('inf'))
 psarr = [robot.getDevice('ps' + str(i)) for i in range(8)]
 [ps.enable(timestep) for ps in psarr]
 
+sideRule = 0 #0 - right, 1 - left
+
 def getDistance(index):
     val = psarr[index].getValue()
     #print(index, val)
     return val
 
+
 def setVelocity(left, right):
-    if left >= 0 and left <= MAX_SPEED and right >= 0 and right <= MAX_SPEED:
         leftWheel.setVelocity(left)
         rightWheel.setVelocity(right)
-    else:
-        if left < 0:
-            leftWheel.setVelocity(0)
-        if right < 0:
-            rightWheel.setVelocity(0)
-        if left > MAX_SPEED:
-            leftWheel.setVelocity(MAX_SPEED)
-        if right > MAX_SPEED:
-            rightWheel.setVelocity(MAX_SPEED)
 
-setVelocity(MAX_SPEED, MAX_SPEED)
-while robot.step(timestep) != -1:
-    if getDistance(2) < 90:
-        break
-        
-setVelocity(MAX_SPEED, 0)
-while robot.step(timestep) != -1:
-    if getDistance(1) > 90:
-        break
         
 while robot.step(timestep) != -1:
     if receiver.getQueueLength() > 0:
-        message = struct.unpack('i', receiver.getData())
+        message = struct.unpack('i', receiver.getData())[0]
         if message not in checkpoints:
             print('adding checkpoint #', message)
             checkpoints.append(message)
-        #print(message)
+        sideRule = message
         receiver.nextPacket()
-
-    if getDistance(0) > 100 or getDistance(7) > 100:
-        setVelocity(MAX_SPEED - (MAX_SPEED / 75 * ((getDistance(0) + getDistance(7)) / 2)), MAX_SPEED)
-    elif getDistance(2) > 400 or getDistance(1) > 400:
-        setVelocity(MAX_SPEED - (MAX_SPEED / 75 * getDistance(2)), MAX_SPEED)
-    elif getDistance(0) < 100 and getDistance(1) < 100:
-        setVelocity(MAX_SPEED, MAX_SPEED - (MAX_SPEED / 100 * getDistance(0)))
+        
+    if sideRule % 2 == 0:
+        if getDistance(0) > 100 or getDistance(7) > 100 or getDistance(1) > 250:
+            setVelocity(0, MAX_SPEED)
+        elif getDistance(2) < 150 and getDistance(1) < 80:
+            setVelocity(MAX_SPEED, 0)
+        elif getDistance(2) > 200:
+            setVelocity(0, MAX_SPEED)
+        else:
+            setVelocity(MAX_SPEED, MAX_SPEED)
     else:
-        setVelocity(MAX_SPEED, MAX_SPEED)
+        if getDistance(0) > 100 or getDistance(7) > 100 or getDistance(6) > 250:
+            setVelocity(MAX_SPEED, 0)
+        elif getDistance(5) < 150 and getDistance(6) < 80:
+            setVelocity(0, MAX_SPEED)
+        elif getDistance(5) > 200:
+            setVelocity(MAX_SPEED, 0)
+        else:
+            setVelocity(MAX_SPEED, MAX_SPEED)
         
 setVelocity(0, 0)
