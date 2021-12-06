ca67011359c8a42cadfe1050360d0eb0956c926f
AuksePaskeviciute <aukse.paskeviciute@ktu.edu>
2021-12-06 11:25:08 +0200

    deleted unused controllers, renamed robot controller, chaged it both webots worlds, so the robot uses the new-named controller.

--- controllers/maze/build/release/maze.d
+++
@@ -1,9 +0,0 @@
-build/release/maze.o: maze.c \
- C:\Program\ Files\Webots/include/controller/c/webots/robot.h \
- C:\Program\ Files\Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/c/webots/nodes.h \
- C:\Program\ Files\Webots/include/controller/c/webots/distance_sensor.h \
- C:\Program\ Files\Webots/include/controller/c/webots/led.h \
- C:\Program\ Files\Webots/include/controller/c/webots/camera.h \
- C:\Program\ Files\Webots/include/controller/c/webots/camera_recognition_object.h \
- C:\Program\ Files\Webots/include/controller/c/webots/compass.h
